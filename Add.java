86c93b79f83e02ec75101a7525809f47c477c4f4
ShibaniPraiseline <shibani.cse.27@gmail.com>
2025-08-16 10:21:01 +0530

    added Add.java

---
+++ Add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = a + b;   // add the two numbers
+        System.out.println(c);  // print result
+    }
+}
